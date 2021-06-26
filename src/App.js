c28601b08c788518efa75cecb82fb93755c12eed
Parada <paradafaii28@gmail.com>
2021-06-26 08:36:59 -0400

    add use effect and adding animation in css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Modal from './Modal/Modal';
 
 function App() {
   const [show, setShow] = useState(false);
+
   return (
     <div className="App">
         <button onClick={() => setShow(true)}>Show Modal</button>
-        <Modal onClose={() => setShow(false)} show={show}/>
+        <Modal title="My Modal" handleClose={() => setShow(false)} show={show}>
+          <p>This is modal body</p>
+        </Modal>
+        {/* show && <Modal onClose={() => setShow(false)} show={show}/> */}
     </div>
   );
 }
--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,26 +1,61 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect } from "react";
+// import PropTypes from "prop-types";
 import "./Modal.css";
+import { CSSTransition } from 'react-transition-group';
+import ReactDOM from 'react-dom'
 
 const Modal = (props) => {
-    if(!props.show) {
-        return null
+    // if(!props.show) {
+    //     return null
+    // }
+
+  const closeOnEscapeKeyDown = (e) => {
+    if ((e.charCode || e.keyCode) === 27) {
+      props.onClose();
     }
-  return (
-    <div className="modal" onClick={props.onClose}>
-      <div className="modal-content" onClick={e => e.stopPropagation()}>
-        <div className="modal-header">
-          <h4 className="modal-title">Modal title</h4>
-        </div>
-        <div className="modal-body">
-            This is modal content
-        </div>
-        <div className="modal-footer">
-            <button onClick={props.onClose} className="button">Close</button>
+  };
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", closeOnEscapeKeyDown);
+    return function cleanup() {
+      document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
+    };
+  }, []);
+
+
+  return ReactDOM.createPortal(
+    <CSSTransition
+      in={props.show}
+      unmountOnExit
+      timeout={{ enter: 0, exit: 300 }}
+    >
+      <div
+        className={`modal ${props.show ? "show" : ""}`}
+        onClick={props.handleClose}
+      >
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <div className="modal-header">
+            <h4 className="modal-title">{props.title || "This is my Modal"}</h4>
+          </div>
+          <div className="modal-body">{props.children}</div>
+          <div className="modal-footer">
+            <button onClick={props.handleClose} className="button">
+              Close
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </CSSTransition>,
+    document.getElementById("root")
   );
 };
+
+// Modal.PropTypes = {
+//   title: PropTypes.string
+// }
+
+// Modal.defaultProps = {
+//   title: 'This is my modal'
+// }
 
 export default Modal;
